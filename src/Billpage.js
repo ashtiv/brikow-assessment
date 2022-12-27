1b1d4560b5f9c3d5a41e1289d7bdce9a9ae46786
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-27 14:50:01 +0530

    tailwind table done

--- src/Billpage.js
+++ src/Billpage.js
@@ -2,18 +2,21 @@ import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
 const generatePDF = () => {
-    const doc = new jsPDF({
-        orientation: 'p',
-        unit: 'px',
-        format: 'a4',
-        hotfixes: ['px_scaling'],
-    });
+
     html2canvas(document.getElementById('my-element'), {
-        width: 800,
-        height: 1600
     }).then((canvas) => {
         const img = canvas.toDataURL("image/png");
-        doc.addImage(img, "PNG", 0, 0, 800, 1600);
+        // const doc = new jsPDF({
+        //     orientation: 'p',
+        //     unit: 'px',
+        //     hotfixes: ['px_scaling'],
+        // });
+        var doc = new jsPDF("l", "px", [200, 600]);
+
+        var width = doc.internal.pageSize.getWidth();
+        var height = doc.internal.pageSize.getHeight();
+        // doc.addImage(img, "PNG", 0, 0, 600, 200);
+        doc.addImage(img, 'JPEG', 0, 0, width, height);
         doc.save("p&lstatement.pdf");
     })
 }
@@ -27,36 +30,75 @@ const DownloadButton = () => {
 const Billpage = () => {
     return (
         <div>
-            <table id='my-element' style={{ height: '400px', width: '800px', padding: '20px' }}>
-                <caption>The Three Most Popular JavaScript Libraries</caption>
-                <thead>
-                    <tr>
-                        <th>Library</th>
-                        <th>jQuery</th>
-                        <th>Bootstrap</th>
-                        <th>Modernizr</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Market Share</td>
-                        <td>96.1%</td>
-                        <td>17.0%</td>
-                        <td>14.3%</td>
-                    </tr>
-                    <tr>
-                        <td>Absolute Usage</td>
-                        <td>70.4%</td>
-                        <td>12.4%</td>
-                        <td>10.5%</td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="4"><em>Market Share</em> refers to the percentage of sites using any JavaScript library that use the specified library. <em>Absolute Usage</em> is the percent of websites surveyed, including those that use no JavaScript libraries, that use the specified library. All data comes from <a href="https://w3techs.com/technologies/overview/javascript_library/all" target="_blank">W3Techs</a> and was accurate in June of 2016.</td>
-                    </tr>
-                </tfoot>
-            </table>
+
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <h2>Responsive Table with RWD-Table-Patterns</h2>
+
+            <div id='my-element' class="container p-10" style={{ height: "400px", width: "1200px" }}>
+                <div class="flex flex-col">
+                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="overflow-hidden">
+                                <table class="min-w-full border text-center">
+                                    <thead class="border-b">
+                                        <tr>
+                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                                Serial No.
+                                            </th>
+                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                                First
+                                            </th>
+                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                                Last
+                                            </th>
+                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                                Handle
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="border-b">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1</td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Mark
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Otto
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                @mdo
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Jacob
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Thornton
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                @fat
+                                            </td>
+                                        </tr>
+                                        <tr class="bg-white border-b">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">3</td>
+                                            <td colspan="2" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r">
+                                                Larry the Bird
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                @twitter
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <DownloadButton />
         </div>
     );

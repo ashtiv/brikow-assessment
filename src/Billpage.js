af87f205b55b5a261edf1182cb7d52a16f4841cc
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-27 22:08:23 +0530

    thead done

--- src/Billpage.js
+++ src/Billpage.js
@@ -41,26 +41,47 @@ const Billpage = () => {
                     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                             <div class="overflow-hidden">
-                                <table class="min-w-full border text-center">
-                                    <thead class="border-b">
+                                <table class="min-w-full border text-center border-solid border-2 border-black">
+                                    <thead class="border-b font-bold border-solid border-2 border-black">
                                         <tr>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
                                                 Serial No.
                                             </th>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                                First
+                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                                Description
                                             </th>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                                Last
+                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                                Unit
                                             </th>
-                                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                                                Handle
+                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                                Rate
+                                            </th>
+                                            <table className='flex'>
+                                                <th scope="col" class="text-sm text-gray-900 border-r flex flex-col w-1/2  border-black">
+                                                    <tr class="text-sm text-gray-900  w-full border-b p-2  border-black">Quantity</tr>
+                                                    <tr class="text-sm text-gray-900  w-full flex ">
+                                                        <tr className='w-1/3 h-full border-r p-2 border-black'>Previous</tr>
+                                                        <tr className='w-1/3 h-full border-r p-2 border-black'>This</tr>
+                                                        <tr className='w-1/3 h-full p-2 border-black'>Cumulative</tr>
+                                                    </tr>
+                                                </th>
+                                                <th scope="col" class="text-sm text-gray-900 border-r flex flex-col w-1/2  border-black">
+                                                    <tr class="text-sm text-gray-900  w-full border-b p-2  border-black">Amount</tr>
+                                                    <tr class="text-sm text-gray-900  w-full flex">
+                                                        <tr className='w-1/3 h-full border-r p-2  border-black'>Previous</tr>
+                                                        <tr className='w-1/3 h-full border-r p-2  border-black'>This</tr>
+                                                        <tr className='w-1/3 h-full p-2  border-black'>Cumulative</tr>
+                                                    </tr>
+                                                </th>
+                                            </table>
+                                            <th class="text-sm text-gray-900 text-center">
+                                                Remarks
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr class="border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">A</td>
                                             <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
                                                 Mark
                                             </td>
@@ -72,7 +93,7 @@ const Billpage = () => {
                                             </td>
                                         </tr>
                                         <tr class="bg-white border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1</td>
                                             <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
                                                 Jacob
                                             </td>
@@ -84,7 +105,7 @@ const Billpage = () => {
                                             </td>
                                         </tr>
                                         <tr class="bg-white border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">3</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
                                             <td colspan="2" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r">
                                                 Larry the Bird
                                             </td>
@@ -100,7 +121,7 @@ const Billpage = () => {
                 </div>
             </div>
             <DownloadButton />
-        </div>
+        </div >
     );
 }
 

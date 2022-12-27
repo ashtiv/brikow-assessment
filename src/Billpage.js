d0d5f608f8bbf7b1be71bf79d69cda51878dfebf
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-27 22:45:25 +0530

    2nd line partial

--- src/Billpage.js
+++ src/Billpage.js
@@ -36,80 +36,94 @@ const Billpage = () => {
             </h1>
             <h2>Responsive Table with RWD-Table-Patterns</h2>
 
-            <div id='my-element' class="container p-10" style={{ height: "400px", width: "1200px" }}>
+            <div id='my-element' class="container p-10 text-xs" style={{ height: "400px !important", width: "1200px !important" }}>
                 <div class="flex flex-col">
-                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                            <div class="overflow-hidden">
+                    <div class="">
+                        <div class="py-2 inline-block min-w-full">
+                            <div class="">
                                 <table class="min-w-full border text-center border-solid border-2 border-black">
                                     <thead class="border-b font-bold border-solid border-2 border-black">
                                         <tr>
-                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                            <th scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
                                                 Serial No.
                                             </th>
-                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                            <th scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
                                                 Description
                                             </th>
-                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                            <th scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
                                                 Unit
                                             </th>
-                                            <th scope="col" class="text-sm text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                            <th scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
                                                 Rate
                                             </th>
                                             <table className='flex'>
-                                                <th scope="col" class="text-sm text-gray-900 border-r flex flex-col w-1/2  border-black">
-                                                    <tr class="text-sm text-gray-900  w-full border-b p-2  border-black">Quantity</tr>
-                                                    <tr class="text-sm text-gray-900  w-full flex ">
+                                                <th scope="col" class="text-xs text-gray-900 border-r flex flex-col w-1/2  border-black">
+                                                    <tr class="text-xs text-gray-900  w-full border-b p-2  border-black">Quantity</tr>
+                                                    <tr class="text-xs text-gray-900  w-full flex ">
                                                         <tr className='w-1/3 h-full border-r p-2 border-black'>Previous</tr>
                                                         <tr className='w-1/3 h-full border-r p-2 border-black'>This</tr>
                                                         <tr className='w-1/3 h-full p-2 border-black'>Cumulative</tr>
                                                     </tr>
                                                 </th>
-                                                <th scope="col" class="text-sm text-gray-900 border-r flex flex-col w-1/2  border-black">
-                                                    <tr class="text-sm text-gray-900  w-full border-b p-2  border-black">Amount</tr>
-                                                    <tr class="text-sm text-gray-900  w-full flex">
+                                                <th scope="col" class="text-xs text-gray-900 border-r flex flex-col w-1/2  border-black">
+                                                    <tr class="text-xs text-gray-900  w-full border-b p-2  border-black">Amount</tr>
+                                                    <tr class="text-xs text-gray-900  w-full flex">
                                                         <tr className='w-1/3 h-full border-r p-2  border-black'>Previous</tr>
                                                         <tr className='w-1/3 h-full border-r p-2  border-black'>This</tr>
                                                         <tr className='w-1/3 h-full p-2  border-black'>Cumulative</tr>
                                                     </tr>
                                                 </th>
                                             </table>
-                                            <th class="text-sm text-gray-900 text-center">
+                                            <th class="text-xs text-gray-900 text-center">
                                                 Remarks
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr class="border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">A</td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                                Mark
+                                            <td scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+                                                A
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                                Otto
+                                            <td scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                @mdo
+                                            <td scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+
+                                            </td>
+                                            <td scope="col" class="text-xs text-gray-900 px-6 py-4 border-r border-solid border-2 border-black">
+
+                                            </td>
+                                            <table className='flex h-full'>
+                                                <td className='w-1/3 h-full border-r p-2 border-black'>Previous</td>
+                                                <td className='w-1/3 h-full border-r p-2 border-black'>This</td>
+                                                <td className='w-1/3 h-full p-2 border-black border-r'>Cumulative</td>
+                                                <td className='w-1/3 h-full border-r p-2 border-black'>Previous</td>
+                                                <td className='w-1/3 h-full border-r p-2 border-black'>This</td>
+                                                <td className='w-1/3 h-full p-2 border-black border-r'>Cumulative</td>
+
+                                            </table>
+                                            <td class="text-xs text-gray-900 text-center">
+
                                             </td>
                                         </tr>
                                         <tr class="bg-white border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1</td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                            <td class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-900 border-r">1</td>
+                                            <td class="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
                                                 Jacob
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                            <td class="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
                                                 Thornton
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td class="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 @fat
                                             </td>
                                         </tr>
                                         <tr class="bg-white border-b">
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">2</td>
-                                            <td colspan="2" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r">
+                                            <td class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-900 border-r">2</td>
+                                            <td colspan="2" class="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center border-r">
                                                 Larry the Bird
                                             </td>
-                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <td class="text-xs text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                 @twitter
                                             </td>
                                         </tr>
